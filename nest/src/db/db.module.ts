5736a96dfbdeae61d324fe25e0bb2160e6bdf1e0
fcole90 <fcole90@gmail.com>
2023-02-25 19:17:44 +0100

    Add support for test DB

--- nest/src/db/db.module.ts
+++ nest/src/db/db.module.ts
@@ -3,5 +3,10 @@ import { ENV } from 'src/constants';
 import { MONGODB_LOCAL_URL, MONGODB_TEST_URL } from './constants';
 
 export const DbModule = MongooseModule.forRoot(
-  process.env.NODE_ENV === ENV.dev ? MONGODB_LOCAL_URL : MONGODB_TEST_URL,
+  process.env.NODE_ENV === ENV.dev
+    ? MONGODB_LOCAL_URL
+    : MONGODB_TEST_URL.replace(
+        '<password>',
+        process.env.MONGODB_TEST_PASS ?? '',
+      ),
 );
