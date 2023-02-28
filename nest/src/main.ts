9afb3b871bf958f7bba401ee43786fa99ac302e1
fcole90 <fcole90@gmail.com>
2023-02-28 22:41:12 +0100

    Add wait for mongoose ready

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 console.log(`Starting on env: '${process.env.NODE_ENV}'`);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.init();
-
+  const appService: AppService = app.get(AppService);
+  await appService.waitConnectionReady();
   console.log('Listen');
   await app.listen(process.env.PORT ?? 3001);
 }
