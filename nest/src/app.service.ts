805b1f91385fd8178f0a42c5a48d6504ccdf8bdd
fcole90 <fcole90@gmail.com>
2023-02-28 22:18:58 +0100

    Log when listening and when connecting

--- nest/src/app.service.ts
+++ nest/src/app.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Injectable()
 export class AppService {
+  constructor(@InjectConnection() private connection: Connection) {
+    console.log(
+      'Mongoose is ',
+      connection.readyState === 1 ? 'ready' : 'not ready',
+    );
+  }
+
+  async waitConnectionReady() {
+    while (!this.connection.readyState) {
+      console.log('Waiting for DB connection');
+      await new Promise((r) => setTimeout(r, 2000));
+    }
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
--- nest/src/main.ts
+++ nest/src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 console.log(`Starting on env: '${process.env.NODE_ENV}'`);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log('Listen');
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
