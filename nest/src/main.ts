76f22ce274a8333d906f36836754b687f2df1347
fcole90 <fcole90@gmail.com>
2023-03-04 12:53:54 +0100

    Remove logs and test simplified DB connection

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -2,7 +2,6 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
-import { DBService } from './db/db.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,16 +9,16 @@ async function bootstrap() {
   });
   const logger = new Logger('bootstrap');
 
-  logger.log(`Starting on env: '${process.env.NODE_ENV}'`);
-  await app.init();
+  // logger.log(`Starting on env: '${process.env.NODE_ENV}'`);
+  // await app.init();
 
-  logger.log(`Waiting for database connection...`);
-  const dbService: DBService = app.get(DBService);
-  while ((await dbService.isConnectionReady()) === false) {
-    logger.log('Database connection not ready, waiting...');
-    await new Promise((r) => setTimeout(r, 50));
-  }
-  logger.log(`Database connection ready`);
+  // logger.log(`Waiting for database connection...`);
+  // const dbService: DBService = app.get(DBService);
+  // while ((await dbService.isConnectionReady()) === false) {
+  //   logger.log('Database connection not ready, waiting...');
+  //   await new Promise((r) => setTimeout(r, 50));
+  // }
+  // logger.log(`Database connection ready`);
 
   const port = process.env.PORT ?? 3001;
   logger.log(`Listening on port: ${port}`);
