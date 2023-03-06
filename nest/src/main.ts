de79c6262a0ad2ffd240b9f55571d11b9392a61d
fcole90 <fcole90@gmail.com>
2023-03-06 20:23:41 +0100

    Add cors

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -2,23 +2,25 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { DBService } from './db/db.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
+  app.enableCors();
   const logger = new Logger('bootstrap');
 
-  // logger.log(`Starting on env: '${process.env.NODE_ENV}'`);
-  // await app.init();
+  logger.log(`Starting on env: '${process.env.NODE_ENV}'`);
+  await app.init();
 
-  // logger.log(`Waiting for database connection...`);
-  // const dbService: DBService = app.get(DBService);
-  // while ((await dbService.isConnectionReady()) === false) {
-  //   logger.log('Database connection not ready, waiting...');
-  //   await new Promise((r) => setTimeout(r, 50));
-  // }
-  // logger.log(`Database connection ready`);
+  logger.log(`Waiting for database connection...`);
+  const dbService: DBService = app.get(DBService);
+  while ((await dbService.isConnectionReady()) === false) {
+    logger.log('Database connection not ready, waiting...');
+    await new Promise((r) => setTimeout(r, 50));
+  }
+  logger.log(`Database connection ready`);
 
   const port = process.env.PORT ?? 3001;
   logger.log(`Listening on port: ${port}`);
